e9b877651f8ce0d6dfd78233bfbbc68f4c941ea2
bowbowzai <jupeng2015@gmail.com>
2023-06-06 22:18:05 +0800

    feat: provider dashboard

--- packages/nextjs/components/vayam-ai/provider/ProviderPreview.tsx
+++ packages/nextjs/components/vayam-ai/provider/ProviderPreview.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { escrow } from "../../../constant/abi.json";
+import { escrow, token } from "../../../constant/abi.json";
 import Loading from "../Loading";
 import { useQuery } from "@tanstack/react-query";
-import { useContract, useProvider, useSigner } from "wagmi";
+import { ethers } from "ethers";
+import { toast } from "react-hot-toast";
+import { useAccount, useContract, useProvider, useSigner } from "wagmi";
 import { getAllDeals } from "~~/api/vayam-ai/deal";
+import { useDeployedContractInfo } from "~~/hooks/scaffold-eth";
 import { Deal } from "~~/types/vayam-ai/Deals";
 import { Proposal } from "~~/types/vayam-ai/Proposal";
 import { TaskItem } from "~~/types/vayam-ai/Task";
@@ -16,6 +19,8 @@ interface TaskPreviewProps {
 }
 
 const ProviderPreview = ({ item }: TaskPreviewProps) => {
+  const { address } = useAccount();
+
   const { data: signer } = useSigner();
   const provider = useProvider();
 
@@ -23,13 +28,23 @@ const ProviderPreview = ({ item }: TaskPreviewProps) => {
   const [numberOfReleased, setNumberOfReleased] = useState(0);
   const [milestoneAmounts, setMilestoneAmounts] = useState([]);
   const [isFetchingData, setIsFetchingData] = useState(false);
+  const [tokenContractAddr, setTokenContractAddr] = useState("");
+  const [invoiceBalance, setInvoiceBalance] = useState("-1");
 
   /*************************************************************
    * Contract interaction
    ************************************************************/
+  const { data: USDCContract } = useDeployedContractInfo("USDC");
+  const { data: DAIContract } = useDeployedContractInfo("DAI");
+
   const escrowContract = useContract({
     address: (deal?.address as string) || "",
     abi: escrow,
+    signerOrProvider: signer || provider,
+  });
+  const tokenContract = useContract({
+    address: (tokenContractAddr as string) || "",
+    abi: token,
     signerOrProvider: signer || provider,
   });
 
@@ -37,7 +52,7 @@ const ProviderPreview = ({ item }: TaskPreviewProps) => {
    * Backend interaction
    ************************************************************/
   const allDealsQuery = useQuery({
-    queryKey: ["providerTaskPreviewDeal"],
+    queryKey: ["providerTaskPreviewDeal", item.proposal.id],
     queryFn: () => getAllDeals(),
     onSuccess: data => {
       const dealRes = data.deals.find((deal: Deal) => deal.task_id == item.task.id);
@@ -49,18 +64,26 @@ const ProviderPreview = ({ item }: TaskPreviewProps) => {
     setIsFetchingData(true);
     try {
       const amounts = await escrowContract?.getAmounts();
-      const released = await escrowContract?.released();
-      setMilestoneAmounts(amounts.length);
-      setNumberOfReleased(released);
+      const milestone = await escrowContract?.milestone(); // get current milestone
+      const token = await escrowContract?.token();
+      setMilestoneAmounts(amounts);
+      setNumberOfReleased(milestone);
+      setTokenContractAddr(token);
+      console.log(`${item.proposal.id}-${milestone}`);
+
+      if (deal?.address) {
+        const invoiceBalance = await tokenContract?.balanceOf(deal?.address);
+        setInvoiceBalance(invoiceBalance);
+      }
     } catch (error) {
-      // toast.error("Get invoice details failed!");
+      toast.error("Get invoice details failed!");
     }
     setIsFetchingData(false);
   }
 
   useEffect(() => {
     getInvoiceDetails();
-  }, [deal?.address]);
+  }, [tokenContractAddr, address, escrowContract, item]);
 
   return (
     <div>
@@ -87,48 +110,73 @@ const ProviderPreview = ({ item }: TaskPreviewProps) => {
           <div className="mt-5">{item.task?.description}</div>
           {/* Ongoing Deal */}
           <div className="mt-5">
-            <div className="text-2xl font-semibold">Ongoing Deal</div>
-            {/* progress bar */}
-            <div className="mt-5">
-              <div className="h-[10px] w-full bg-white rounded-full">
-                <div
-                  style={{
-                    width: `${
-                      parseInt(String(numberOfReleased)) == 0
-                        ? 0
-                        : (parseInt(String(numberOfReleased)) / parseInt(String(milestoneAmounts?.length))) * 100
-                    }%`,
-                  }}
-                  className="h-[10px] bg-sideColor rounded-full"
-                ></div>
-              </div>
-              <div>
-                {milestoneAmounts?.map((amount, index) => {
-                  return (
+            {/* Ongoing Deal */}
+            {item.proposal.accepted == true && milestoneAmounts?.length > 0 ? (
+              <div className="mt-5">
+                <div className="text-2xl font-semibold">Ongoing Deal</div>
+                {/* progress bar */}
+                <div className="mt-5">
+                  <div className="h-[10px] w-full bg-white rounded-full">
                     <div
-                      key={parseInt(String(amount)) + index}
                       style={{
-                        width: `${100 / parseInt(String(milestoneAmounts?.length))}%`,
+                        width: `${
+                          parseInt(String(numberOfReleased)) == 0
+                            ? 0
+                            : (parseInt(String(numberOfReleased)) / parseInt(String(milestoneAmounts?.length))) * 100
+                        }%`,
                       }}
-                      className="inline-block text-sideColor"
-                    >
-                      ${parseInt(String(amount))}
-                    </div>
-                  );
-                })}
+                      className="h-[10px] bg-sideColor rounded-full"
+                    ></div>
+                  </div>
+                  <div>
+                    {milestoneAmounts?.map((amount, index) => {
+                      return (
+                        <div
+                          key={index}
+                          style={{
+                            width: `${100 / parseInt(String(milestoneAmounts?.length))}%`,
+                          }}
+                          className="inline-block text-sideColor"
+                        >
+                          ${ethers.utils.formatEther(String(amount))}
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+                <div>
+                  Total Value Locked:{" "}
+                  {invoiceBalance != "-1" && invoiceBalance != undefined
+                    ? ethers.utils.formatEther(
+                        invoiceBalance != "-1" && invoiceBalance != undefined ? invoiceBalance : "0",
+                      )
+                    : "0"}
+                  {tokenContractAddr == USDCContract?.address
+                    ? "USDC"
+                    : tokenContractAddr == DAIContract?.address
+                    ? "DAI"
+                    : ""}
+                </div>
               </div>
-            </div>
+            ) : (
+              <div></div>
+            )}
             {/* milestones */}
             <div className="mt-5 text-2xl font-semibold">Proposed Milestones</div>
             <div></div>
             <div className="mt-3 flex flex-col gap-3">
-              {item.proposal.milestones.map(proposal => (
+              {item.proposal.milestones.map((proposal, index) => (
                 <div key={proposal.description} className="grid grid-cols-4 items-center">
                   <div>{proposal.description}</div>
                   <div className="text-sideColor">${proposal.price}</div>
-                  <div className="place-self-start flex items-center gap-1">
-                    <div className="cursor-pointer connect-bg text-center rounded-lg w-fit px-7 py-1">Submit</div>
-                  </div>
+                  {item.proposal.accepted &&
+                    (index < numberOfReleased ? (
+                      <div className="text-green-300 col-span-2 text-center w-fit px-7 py-1">Released</div>
+                    ) : (
+                      <div className="place-self-start flex items-center gap-1">
+                        <div className="cursor-pointer connect-bg text-center rounded-lg w-fit px-7 py-1">Submit</div>
+                      </div>
+                    ))}
                 </div>
               ))}
             </div>
